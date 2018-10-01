c1ea37bdfd6ad559609ee2b21cfd6ebeae5852db
Joao Pedro Cardoso <joaopedrocar@hotmail.com>
2018-10-01 10:10:35 -0300

    Initial Commit

---
+++ food-control/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  food-control
+//
+//  Created by João Pedro Cardoso de Araújo on 01/10/18.
+//  Copyright © 2018 João Pedro Cardoso de Araújo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ food-controlTests/food_controlTests.swift
@@ -0,0 +1,34 @@
+//
+//  food_controlTests.swift
+//  food-controlTests
+//
+//  Created by João Pedro Cardoso de Araújo on 01/10/18.
+//  Copyright © 2018 João Pedro Cardoso de Araújo. All rights reserved.
+//
+
+import XCTest
+@testable import food_control
+
+class food_controlTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
